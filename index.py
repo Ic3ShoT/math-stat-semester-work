03e847b63010c6e3b0e39742be103f2a466589bf
Aishat Zakirov <lickey8crp@gmail.com>
2022-04-25 21:55:55 +0300

    firstscript

--- index.py
+++ index.py
@@ -3,8 +3,20 @@ import pandas
 import matplotlib
 import scipy
 import plot
-
+array_iris=[]
 with open('iris.data') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
             print(", ".join(row))
+            array_iris.append(row)
+i=0
+first_column=[]
+for i in range(0,30):
+    first_column.append(float((array_iris[i])[0]))
+    i=i+1
+first_column_max=first_column[0]
+for chislo in first_column:
+    if chislo>first_column_max:
+        first_column_max=chislo
+print(first_column)
+print(first_column_max)
