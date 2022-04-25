28efa7ca35be453f43f70c26fd9f23f88a828eb0
Aishat Zakirov <lickey8crp@gmail.com>
2022-04-25 21:32:55 +0300

    parser

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 import csv
+import pandas
+import matplotlib
+import scipy
+import plot
+
+with open('iris.data') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+            print(", ".join(row))
